3eb226f8a2e1a2bc68fc120d869477e20eefcd3b
joeylaguna <elagunaj@gmail.com>
2017-05-08 21:50:44 -0700

    Change path of compiled jsx, update path in index

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 var webpack = require('webpack');
 var path = require('path');
-var BUILD_DIR = path.resolve(__dirname, 'public');
+var BUILD_DIR = path.resolve(__dirname, 'public/compiled');
 var APP_DIR = path.resolve(__dirname, 'public/components');
 var config = {
   entry: APP_DIR + '/index.jsx',
