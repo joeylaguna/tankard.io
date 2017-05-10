38d535e6b3af55dbf3189db76d08cd439641faa3
joeylaguna <elagunaj@gmail.com>
2017-05-10 12:56:10 -0700

    Add object to map glasses to amazon link

---
+++ glassShopList.js
@@ -0,0 +1,14 @@
+const glassShopList = {
+  'Flute Glass': 'https://www.amazon.com/Libbey-Stemless-Flute-Glasses-Clear/dp/B003XVKNVO/ref=sr_1_2?ie=UTF8&qid=1494376606&sr=8-2&keywords=flute+glass',
+  'Goblet (or Chalice)': 'https://www.amazon.com/Libbey-Schooner-Beer-Glass-21/dp/B00B4DC9QA/ref=sr_1_2?s=kitchen&ie=UTF8&qid=1494376710&sr=1-2&keywords=glass+goblet',
+  'Mug (or Seidel, Stein)': 'https://www.amazon.com/Dimple-Stein-Beer-Mug-Pack/dp/B00V5F2G5G/ref=sr_1_1?s=home-garden&ie=UTF8&qid=1494376770&sr=1-1&keywords=glass+mug+beer',
+  'Pilsner Glass (or Pokal)': 'https://www.amazon.com/Style-Setter-Soho-Pilsner-Glasses/dp/B004RHVYWU/ref=sr_1_2?s=home-garden&ie=UTF8&qid=1494376805&sr=1-2&keywords=Pilsner+Glass',
+  'Pint Glass (or Becker, Nonic, Tumbler)': 'https://www.amazon.com/Bormioli-Rocco-Nonix-Glass-19-75oz/dp/B000P4F91G/ref=sr_1_27?s=home-garden&ie=UTF8&qid=1494376848&sr=1-27&keywords=pint+glass',
+  'Snifter': 'https://www.amazon.com/Libbey-8405S4-4-Piece-Spirits-12-Ounce/dp/B00JB02S3W/ref=sr_1_2?s=home-garden&ie=UTF8&qid=1494376895&sr=1-2&keywords=Snifter',
+  'Stange': '',
+  'Tulip': 'https://www.amazon.com/Tulip-Glass-Connoisseur-Collection-Handwerk/dp/B01N47VE5P/ref=sr_1_1?ie=UTF8&qid=1494377002&sr=8-1&keywords=tulip+glasses+for+beer',
+  'Weizen Glass': 'https://www.amazon.com/Libbey-Craft-Brews-23-Ounce-4-Piece/dp/B008OQNA8Q/ref=sr_1_1?ie=UTF8&qid=1494377030&sr=8-1&keywords=Weizen+Glass',
+  'Oversized Wine Glass': 'https://www.amazon.com/International-Luminarc-Cachet-Glass-20-Ounce/dp/B009ZRTE0G/ref=sr_1_6?ie=UTF8&qid=1494377053&sr=8-6&keywords=Oversized+Wine+Glass'
+}
+
+module.exports =  glassShopList;
