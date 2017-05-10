273c271c6322748dd170fc19e8f9e5271bfb4fa7
joeylaguna <elagunaj@gmail.com>
2017-05-10 12:55:48 -0700

    Add link to buy glass in beerPair component

--- public/components/BeerPair.jsx
+++ public/components/BeerPair.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import beerPair from './../../pairList.js';
+import glassShopList from './../../glassShopList.js';
 
 class BeerPair extends React.Component {
   constructor(props){
@@ -10,7 +11,12 @@ class BeerPair extends React.Component {
   	return(
   	  <div>
   	    {
-					this.props.currentBeer.name ? `${this.props.currentBeer.name} is a ${this.props.currentBeer.type} and should be drank in a ${beerPair[this.props.currentBeer.type][0]}` : ''
+					this.props.currentBeer.name ? 
+					<div>{this.props.currentBeer.name} is a {this.props.currentBeer.type} and should be drank in a {beerPair[this.props.currentBeer.type][0]}.
+						<br/>
+						You can get one <a target='_blank' href={glassShopList[beerPair[this.props.currentBeer.type][0]]}>here!</a>
+					</div>
+					: ''
 				}
   	  </div>
   	)
