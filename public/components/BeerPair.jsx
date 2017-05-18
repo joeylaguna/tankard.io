e2c4647c1d265ba49e2dd5601cf35575693402cc
joeylaguna <elagunaj@gmail.com>
2017-05-17 23:42:31 -0700

    Render glass from current glass state instead of array.

--- public/components/BeerPair.jsx
+++ public/components/BeerPair.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import MulitpleGlasses from './MultipleGlasses.jsx';
 import beerPair from './../../pairList.js';
 import glassShopList from './../../glassShopList.js';
 
@@ -12,8 +13,9 @@ class BeerPair extends React.Component {
   	  <div>
   	    {
 					this.props.currentBeer.name ? 
-					<div>{this.props.currentBeer.name} is a {this.props.currentBeer.type} and should be drank in a {beerPair[this.props.currentBeer.type][0]}.
+					<div>{this.props.currentBeer.name} is a {this.props.currentBeer.type} and can be drank in a {this.props.currentGlass}.
 						<br/>
+						{beerPair[this.props.currentBeer.type].length > 1 ? <MulitpleGlasses updateGlass={this.props.updateGlass} glasses={beerPair[this.props.currentBeer.type]} currentGlass={this.props.currentGlass} /> : ''}
 						You can get one <a target='_blank' href={glassShopList[beerPair[this.props.currentBeer.type][0]]}>here!</a>
 					</div>
 					: ''
