5fc2026669225dd1ffaaa074420d44acfb679374
joeylaguna <elagunaj@gmail.com>
2017-05-17 23:49:52 -0700

    Fix amazon link to correctly link to current glass

--- public/components/BeerPair.jsx
+++ public/components/BeerPair.jsx
@@ -16,7 +16,7 @@ class BeerPair extends React.Component {
 					<div>{this.props.currentBeer.name} is a {this.props.currentBeer.type} and can be drank in a {this.props.currentGlass}.
 						<br/>
 						{beerPair[this.props.currentBeer.type].length > 1 ? <MulitpleGlasses updateGlass={this.props.updateGlass} glasses={beerPair[this.props.currentBeer.type]} currentGlass={this.props.currentGlass} /> : ''}
-						You can get one <a target='_blank' href={glassShopList[beerPair[this.props.currentBeer.type][0]]}>here!</a>
+						You can get one <a target='_blank' href={glassShopList[this.props.currentGlass]}>here!</a>
 					</div>
 					: ''
 				}
