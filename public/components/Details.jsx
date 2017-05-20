471eb0a196d396804f3553caa1a5e66a8e27f702
joeylaguna <elagunaj@gmail.com>
2017-05-19 20:01:02 -0700

    Refactor details component to stateless component

--- public/components/Details.jsx
+++ public/components/Details.jsx
@@ -2,19 +2,12 @@ import React from 'react';
 import glassDetails from './../../glassDetails.js';
 import beerPair from './../../pairList.js';
 
-class Details extends React.Component {
-  constructor(props){
-  	super(props);
-  }
-
-  render(){
-  	return(
-  	  <div>
-        {this.props.updateGlass ? `${glassDetails[this.props.updateGlass]}` : ''}
-  	  </div>
-  	)
-  }
+const Details = (props) => {
+  return (
+    <div>
+      {props.updateGlass ? `${glassDetails[props.updateGlass]}` : ''}
+    </div>  
+  );
 }
-
 
 export default Details;
