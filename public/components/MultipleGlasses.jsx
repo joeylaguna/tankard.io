7738546008330e98047c9845b44b99656d107d0b
joeylaguna <elagunaj@gmail.com>
2017-05-19 20:08:13 -0700

    Refactor multiple glasses component to stateless component

--- public/components/MultipleGlasses.jsx
+++ public/components/MultipleGlasses.jsx
@@ -1,35 +1,27 @@
-import React from 'react';
-import glassDetails from './../../glassDetails.js';
-import beerPair from './../../pairList.js';
+import React         from 'react';
+import glassDetails  from './../../glassDetails.js';
+import beerPair      from './../../pairList.js';
 
-class MultipleGlasses extends React.Component {
-  constructor(props){
-  	super(props);
-    this.handleGlassUpdate = this.handleGlassUpdate.bind(this);
-  }
-
-  handleGlassUpdate(e) {
+const MultipleGlasses = (props) => {
+  const handleGlassUpdate = (e) => {
     e.preventDefault();
-    this.props.updateGlass(e.target.innerHTML);
+    props.updateGlass(e.target.innerHTML);
   }
 
-  render(){
-    let temp = this.props.glasses.slice();
-    temp.splice(temp.indexOf(this.props.currentGlass), 1);
-    const list = temp.map((glass, i) => {
-      return <li key={i} onClick={this.handleGlassUpdate}>{glass}</li>
-    });
-    
-  	return(
-  	  <div>
-        <p>You can also use a: </p>
-        <ul>
-          {list}
-        </ul>
-  	  </div>
-  	)
-  }
+  let temp = props.glasses.slice();
+  temp.splice(temp.indexOf(props.currentGlass), 1);
+  const list = temp.map((glass, i) => {
+    return <li key={i} onClick={handleGlassUpdate}>{glass}</li>
+  });
+ 
+  return (
+    <div>
+      <p>You can also use a: </p>
+      <ul>
+        {list}
+      </ul>
+   </div>
+  );
 }
-
 
 export default MultipleGlasses;
