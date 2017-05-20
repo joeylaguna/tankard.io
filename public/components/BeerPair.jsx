f2b3e50fd62c54e7882195c6857f415d2020c52f
joeylaguna <elagunaj@gmail.com>
2017-05-19 19:58:05 -0700

    Refactor beerpair component to stateless component

--- public/components/BeerPair.jsx
+++ public/components/BeerPair.jsx
@@ -3,27 +3,21 @@ import MulitpleGlasses from './MultipleGlasses.jsx';
 import beerPair from './../../pairList.js';
 import glassShopList from './../../glassShopList.js';
 
-class BeerPair extends React.Component {
-  constructor(props){
-  	super(props);
-  }
-
-  render(){
-  	return(
-  	  <div>
-  	    {
-					this.props.currentBeer.name ? 
-					<div>{this.props.currentBeer.name} is a {this.props.currentBeer.type} and can be drank in a {this.props.currentGlass}.
-						<br/>
-						{beerPair[this.props.currentBeer.type].length > 1 ? <MulitpleGlasses updateGlass={this.props.updateGlass} glasses={beerPair[this.props.currentBeer.type]} currentGlass={this.props.currentGlass} /> : ''}
-						You can get one <a target='_blank' href={glassShopList[this.props.currentGlass]}>here!</a>
-					</div>
-					: ''
-				}
-  	  </div>
-  	)
-  }
+const BeerPair = (props) => {
+	console.log('here');
+	return(
+    <div>
+			{
+				props.currentBeer.name ? 
+				<div>{props.currentBeer.name} is a {props.currentBeer.type} and can be drank in a {props.currentGlass}.
+					<br/>
+					{beerPair[props.currentBeer.type].length > 1 ? <MulitpleGlasses updateGlass={props.updateGlass} glasses={beerPair[props.currentBeer.type]} currentGlass={props.currentGlass} /> : ''}
+					You can get one <a target='_blank' href={glassShopList[props.currentGlass]}>here!</a>
+				</div>
+				: ''
+			}
+		</div>
+	);
 }
-
 
 export default BeerPair;
