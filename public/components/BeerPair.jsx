801cc4cee954011223b10303a75d529a5bef3246
joeylaguna <elagunaj@gmail.com>
2017-05-19 20:19:11 -0700

    Beerpair component code cleanup

--- public/components/BeerPair.jsx
+++ public/components/BeerPair.jsx
@@ -4,20 +4,21 @@ import beerPair from './../../pairList.js';
 import glassShopList from './../../glassShopList.js';
 
 const BeerPair = (props) => {
-	console.log('here');
-	return(
+  console.log('here');
+  return (
     <div>
-			{
-				props.currentBeer.name ? 
-				<div>{props.currentBeer.name} is a {props.currentBeer.type} and can be drank in a {props.currentGlass}.
-					<br/>
-					{beerPair[props.currentBeer.type].length > 1 ? <MulitpleGlasses updateGlass={props.updateGlass} glasses={beerPair[props.currentBeer.type]} currentGlass={props.currentGlass} /> : ''}
-					You can get one <a target='_blank' href={glassShopList[props.currentGlass]}>here!</a>
-				</div>
-				: ''
-			}
-		</div>
-	);
+	  {
+	    props.currentBeer.name ? 
+		  <div>
+			{props.currentBeer.name} is a {props.currentBeer.type} and can be drank in a {props.currentGlass}.
+			<br/>
+			{beerPair[props.currentBeer.type].length > 1 ? <MulitpleGlasses updateGlass={props.updateGlass} glasses={beerPair[props.currentBeer.type]} currentGlass={props.currentGlass} /> : ''}
+			You can get one <a target='_blank' href={glassShopList[props.currentGlass]}>here!</a>
+		  </div>
+		: ''
+	  }
+	  </div>
+  );
 }
 
 export default BeerPair;
