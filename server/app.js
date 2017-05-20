fa71e316cd3cd31ef6536ac812b66b07d34509d9
joeylaguna <elagunaj@gmail.com>
2017-05-20 07:38:33 -0700

    Fix loading bug

--- server/app.js
+++ server/app.js
@@ -1,16 +1,17 @@
-const express = require('express');
-const cors = require('cors');
-const axios = require('axios');
-const cheerio = require('cheerio');
-const request = require('request');
-const app = express();
+const express           = require('express');
+const cors              = require('cors');
+const axios             = require('axios');
+const cheerio           = require('cheerio');
+const request           = require('request');
+const app               = express();
+
 app.use(cors({origin: 'http://localhost:3000'}));
 
 app.use(express.static(__dirname + '/../public'));
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', (req, res) => {
-  res.send('success');
+  res.send(200);
 });
 
 app.get('/:beer', (req, res) => {
